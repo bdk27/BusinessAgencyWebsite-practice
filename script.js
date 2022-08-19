6c1192ccc64bf674d1b458c2d80cd105041e5ba6
BDK27 <briansam195@gmail.com>
2022-08-19 21:12:52 +0800

    slidebar

--- script.js
+++ script.js
@@ -5,8 +5,18 @@ $(function() {
     });
 });
 
+let menu = document.getElementById('bar');
+let items = document.getElementById('navbar');
 
+items.style.right = '-360px';
 
+menu.onclick = function() {
+    if (items.style.right === '-360px'){
+        items.style.right = '0';
+    }else {
+        items.style.right = '-360px';
+    }
+}
 
 
 
